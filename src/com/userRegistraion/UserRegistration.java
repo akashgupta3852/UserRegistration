37da5f0f67f9fa0a6566bab20cae7618bb9e72dd
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-03 22:10:42 +0530

    [Akash]Refactored: password patterns into single pattern

--- src/com/userRegistraion/UserRegistration.java
+++ src/com/userRegistraion/UserRegistration.java
@@ -5,87 +5,42 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	public static List<String> emailIdList=new ArrayList<>();
-	
-	public static void addEmailInList() {
-		emailIdList.add("abc@yahoo.com");
-		emailIdList.add("abc-100@yahoo.com");
-		emailIdList.add("abc.100@yahoo.com");
-		emailIdList.add("abc111@abc.com");
-		emailIdList.add("abc-100@abc.net");
-		emailIdList.add("abc.100@abc.com.au");
-		emailIdList.add("abc@1.com");
-		emailIdList.add("abc+100@gmail.com");
-		emailIdList.add("abc");
-		emailIdList.add("abc@.com.my");
-		emailIdList.add("abc123@gmail.a");
-		emailIdList.add("abc123@.com");
-		emailIdList.add("abc123@.com.com");
-		emailIdList.add(".abc@abc.com");
-		emailIdList.add("abc()*@gmail.com");
-		emailIdList.add("abc@%*.com");
-		emailIdList.add("abc..2002@gmail.com");
-		emailIdList.add("abc.@gmail.com");
-		emailIdList.add("abc@abc@gmail.com");
-		emailIdList.add("abc@gmail.com.1a");
-		emailIdList.add("abc@gmail.com.aa.au");
-		emailIdList.add("abc@gmail.com.com");
-	}
-	
-	public static void checkAllEmailSamples() {
-		for(String id : emailIdList) {
-			if(isValidEmailId(id))
-				System.out.println("The email id: "+id+" is valid");
-			else
-				System.out.println("Sorry, the email id: "+id+" is invalid");
-		}
-	}
+	private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
+	private static final String EMAIL_ADDRESS_PATTERN="^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
+	private static final String MOBILE_NUMBER_PATTERN="^[0-9]{2}[\\s]{1}[6-9][0-9]{9}$";
+	private static final String PASSWORD_PATTERN="(?=.*[A-Z])(?=.*[0-9]).{8,}";
 	
 	public static boolean isValidFirstName(String firstName) {
-		String regex="^[A-Z][a-z]{2,}";
-		Pattern pattern=Pattern.compile(regex);
+		Pattern pattern=Pattern.compile(NAME_PATTERN);
 		if(pattern.matcher(firstName).matches())
 			return true;
 		return false;
 	}
 
 	public static boolean isValidLastName(String lastName) {
-		String regex="^[A-Z][a-z]{2,}";
-		Pattern pattern=Pattern.compile(regex);
+		Pattern pattern=Pattern.compile(NAME_PATTERN);
 		if(pattern.matcher(lastName).matches())
 			return true;
 		return false;
 	}
 	
 	public static boolean isValidEmailId(String emailId) {
-		String regex="^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2})?$";
-		Pattern pattern=Pattern.compile(regex);
+		Pattern pattern=Pattern.compile(EMAIL_ADDRESS_PATTERN);
 		if(pattern.matcher(emailId).matches())
 			return true;
 		return false;
 	}
 	
 	public static boolean isValidMobileNumber(String mobileNumber) {
-		String regex="^[0-9]{2}[\\s]{1}[6-9][0-9]{9}$";
-		Pattern pattern=Pattern.compile(regex);
+		Pattern pattern=Pattern.compile(MOBILE_NUMBER_PATTERN);
 		if(pattern.matcher(mobileNumber).matches())
 			return true;
 		return false;
 	}
 	
 	public static boolean isValidPassword(String password) {
-		String regex=".{8,}";
-		String regex1="[A-Z]";
-		String regex2="[0-9]";
-		String regex3="\\W";
-		Pattern pattern=Pattern.compile(regex);
-		Pattern pattern1=Pattern.compile(regex1);
-		Pattern pattern2=Pattern.compile(regex2);
-		Pattern pattern3=Pattern.compile(regex3);
+		Pattern pattern=Pattern.compile(PASSWORD_PATTERN);
 		if(pattern.matcher(password).matches())
-			if(pattern1.matcher(password).find())
-				if(pattern2.matcher(password).find())
-					if(pattern3.matcher(password).find())					
 			return true;
 		return false;
 	}
@@ -93,11 +48,6 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Problem");
 
-		System.out.println("Checking all email samples:");
-		addEmailInList();
-		checkAllEmailSamples();
-
-		
 		Scanner scan=new Scanner(System.in);
 		
 		System.out.println("Enter the first name:");
@@ -151,5 +101,4 @@ public class UserRegistration {
 				System.exit(0);;
 			}
 	}
-
 }
